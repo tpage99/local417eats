0690b380ea7bb64a0bde4789c3d8e1bed177284a
Taylor Page <taylor@thepagesmedia.com>
2020-04-19 21:54:16 -0500

    updated filter button for accurate styling and filter categories for appropriately sized tap targets on mobile

--- src/components/filterButton.js
+++ src/components/filterButton.js
@@ -2,14 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 
 const filterButton = () => (
-  <div className="sticky bottom-0 right-0 pb-4">
-    <Link
-      to="/filter/"
-      className="bg-gray-800 hover:bg-gray-600 text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-    >
-      Filter
-    </Link>
-  </div>
+  <Link
+    to="/filter/"
+    className="bg-gray-800 hover:bg-gray-600 text-white text-2xl font-semibold py-2 px-6 border border-gray-400 rounded shadow"
+    style={{ position: `fixed`, bottom: `1rem`, right: `1rem` }}
+  >
+    Filter
+  </Link>
 )
 
 export default filterButton
--- src/pages/filter/index.js
+++ src/pages/filter/index.js
@@ -10,26 +10,26 @@ const FilterPage = () => (
     <h1 className="mb-0 py-2 text-4xl font-bold text-center leading-tight">
       Filter results by...
     </h1>
-    <ul className="pt-4">
-      <li className="mb-4 max-w-sm rounded overflow-hidden shadow-lg border border-gray-400">
-        <Link
-          to="/filter/category/"
-          className="px-6 py-4 font-bold text-xl mb-2"
-        >
-          Category
-        </Link>
-      </li>
-      <li className="mb-4 max-w-sm rounded overflow-hidden shadow-lg border border-gray-400">
-        <Link to="/filter/food/" className="px-6 py-4 font-bold text-xl mb-2">
-          Food
-        </Link>
-      </li>
-      <li className="mb-4max-w-sm rounded overflow-hidden shadow-lg border border-gray-400">
-        <Link to="/filter/area/" className="px-6 py-4 font-bold text-xl mb-2">
-          Area
-        </Link>
-      </li>
-    </ul>
+    <div className="pt-4 flex flex-wrap text-center">
+      <Link
+        to="/filter/category/"
+        className="m-2 max-w-sm rounded overflow-hidden shadow-lg border border-gray-400 px-6 py-4 font-bold text-xl"
+      >
+        Category
+      </Link>
+      <Link
+        to="/filter/food/"
+        className="m-2 max-w-sm rounded overflow-hidden shadow-lg border border-gray-400 px-6 py-4 font-bold text-xl"
+      >
+        Food
+      </Link>
+      <Link
+        to="/filter/area/"
+        className="m-2 max-w-sm rounded overflow-hidden shadow-lg border border-gray-400 px-6 py-4 font-bold text-xl"
+      >
+        Area
+      </Link>
+    </div>
   </Layout>
 )
 
